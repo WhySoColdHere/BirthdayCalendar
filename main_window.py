4b20765c7ec81905190329f77d4c1f238261d50b
WhySoColdHere <lavrfan2005@mail.ru>
2024-11-09 14:11:21 +0300

    submit_stuff

--- main_window.py
+++ main_window.py
@@ -29,7 +29,7 @@ class MainWindow(QWidget):
                 font-weight: 700;
             }
             """
-        self.buttons_width = 170
+        self.buttons_width = 220
         self.buttons_height = 25
         self.buttons_margin = 25
 
@@ -51,50 +51,131 @@ class MainWindow(QWidget):
                 font-size: 20px;
             }
             """
-        self.labels_margin = self.lines_edit_margin
+        self.labels_margin = self.lines_edit_margin + 1
 
         self.background_label = QLabel(self)
-        self.title_label = QLabel(self)
-        self.action_label = QLabel(self)
-
-        self.add_birthday_btn = QPushButton(self)
-        self.get_birthdays_btn = QPushButton(self)
-        self.get_birthday_btn = QPushButton(self)
-        self.edit_birthday_btn = QPushButton(self)
-        self.delete_birthday_btn = QPushButton(self)
-
-        self.main_lbls = [self.title_label, self.action_label]
-
-        self.main_btns = {self.add_birthday_btn: ["Add birthday", self.add_birthday_btn_clicked],
-                          self.get_birthday_btn: ["Get birthday", self.get_birthday_btn_clicked],
-                          self.get_birthdays_btn: ["Get all birthdays", self.get_birthdays_btn_clicked],
-                          self.edit_birthday_btn: ["Edit birthday", self.edit_birthday_btn_clicked],
-                          self.delete_birthday_btn: ["Remove birthday", self.delete_birthday_btn_clicked]}
-
-        self.main_labels()
-        self.main_buttons()
-        self.show()
-
-    def main_labels(self):
-        self.title_label.setText("HAPPY BIRTHDAY!")
-        self.title_label.move(120, 50)
-        self.title_label.setStyleSheet("font-family: 'Lastri'; font-size: 30px;")
-
-        self.action_label.setText("Choose the action!")
-        self.action_label.move(110, 130)
-        self.action_label.setStyleSheet("font-family: 'Lastri'; font-size: 25px")
-
         self.background_label.setPixmap(self.background_image)
 
-    def main_buttons(self):
-        x = 165
-        y = 190
+        self.main_lbls = {
+            QLabel(self): ["HAPPY BIRTHDAY!", "font-family: 'Lastri'; font-size: 30px;", (120, 50)],
+            QLabel(self): ["Choose the action!", "font-family: 'Lastri'; font-size: 25px", (110, 130)]
+        }
 
-        self.set_buttons_settings(self.main_btns)
+        self.main_btns = {
+            QPushButton(self): ["Add birthday", self.add_birthday_btn_clicked],
+            QPushButton(self): ["Get birthday", self.get_birthday_btn_clicked],
+            QPushButton(self): ["Get all birthdays", self.get_birthdays_btn_clicked],
+            QPushButton(self): ["Edit birthday", self.edit_birthday_btn_clicked],
+            QPushButton(self): ["Remove birthday", self.delete_birthday_btn_clicked]
+        }
 
-        for button in self.main_btns.keys():
-            button.move(x, y)
-            y += self.buttons_margin
+        self.add_birthday_lbls = {
+            QLabel(self): "First name",
+            QLabel(self): "Last name",
+            QLabel(self): "Date"
+        }
+
+        self.add_birthday_btns = {
+            QPushButton(self): ["Submit", self.add_birthday_submit_btn_clicked],
+        }
+
+        self.add_birthday_lines_ed = {
+            QLineEdit(self): "Vladimir",
+            QLineEdit(self): "Putin",
+            QLineEdit(self): "07.10.1952"
+        }
+
+        self.back_btn = QPushButton(self)
+
+        self.hide_widgets(
+            self.back_btn,
+            *self.main_btns,
+            *self.main_lbls,
+            *self.add_birthday_lbls.keys(),
+            *self.add_birthday_btns.keys(),
+            *self.add_birthday_lines_ed.keys()
+        )
+
+        self.main_window_gui_show()
+        self.show()
+
+    def main_window_gui_show(self):
+        def main_labels():
+            for label in self.main_lbls:
+                label.setText(self.main_lbls[label][0])
+                label.setStyleSheet(self.main_lbls[label][1])
+                label.move(self.main_lbls[label][2][0], self.main_lbls[label][2][1])
+
+        def main_buttons():
+            x = 140
+            y = 190
+
+            self.set_buttons_settings(self.main_btns)
+
+            for button in self.main_btns.keys():
+                button.move(x, y)
+                y += self.buttons_margin
+
+        main_labels()
+        main_buttons()
+
+        self.show_widgets(
+            *self.main_lbls.keys(),
+            *self.main_btns.keys(),
+        )
+
+    def main_window_gui_hide(self):
+        self.hide_widgets(
+            *self.main_lbls.keys(),
+            *self.main_btns.keys()
+        )
+
+    def add_birthday_gui_show(self):
+        def labels():
+            x = 100
+            y = 160
+
+            self.set_labels_settings(self.add_birthday_lbls)
+            for label in self.add_birthday_lbls.keys():
+                label.move(x, y)
+                y += self.labels_margin
+
+        def buttons():
+            x = 135
+            y = 300
+
+            self.set_buttons_settings(self.add_birthday_btns)
+            for button in self.add_birthday_btns.keys():
+                button.move(x, y)
+                y += self.buttons_margin
+
+        def lines_edit():
+            x = 240
+            y = 160  # 159
+
+            self.set_lines_edit_settings(self.add_birthday_lines_ed)
+            for line_edit in self.add_birthday_lines_ed.keys():
+                line_edit.move(x, y)
+                y += self.lines_edit_margin
+
+        labels()
+        buttons()
+        lines_edit()
+
+        self.back_button(self.back_btn, self.add_birthday_gui_hide, self.main_window_gui_show)
+
+        self.show_widgets(
+            *self.add_birthday_lbls.keys(),
+            *self.add_birthday_btns.keys(),
+            *self.add_birthday_lines_ed.keys()
+        )
+
+    def add_birthday_gui_hide(self):
+        self.hide_widgets(
+            *self.add_birthday_lines_ed.keys(),
+            *self.add_birthday_btns.keys(),
+            *self.add_birthday_lbls.keys()
+        )
 
     def set_buttons_settings(self, current_buttons):
         for button in current_buttons.keys():
@@ -108,73 +189,58 @@ class MainWindow(QWidget):
             label.setStyleSheet(self.labels_stylesheet)
             label.setText(current_labels[label])
 
+    def set_lines_edit_settings(self, current_lines_edit):
+        for line_edit in current_lines_edit.keys():
+            line_edit.setStyleSheet(self.lines_edit_stylesheet)
+            line_edit.setPlaceholderText(current_lines_edit[line_edit])
+
+    def back_button(self, back_btn, *args):
+        x = 200
+        y = 50
+
+        back_btns = {self.back_btn: ["Back", lambda: self.back_btn_clicked(back_btn, args)]}
+        self.set_buttons_settings(back_btns)
+        self.back_btn.move(x, y)
+        self.back_btn.show()
+
+    @staticmethod
+    def back_btn_clicked(back_btn, args):
+        for func in args:
+            func()
+        back_btn.hide()
+
+    def add_birthday_submit_btn_clicked(self):
+        pass
+        # self.add_birthday_gui_hide()
+        #
+        # try:
+        #     lines_edit = []
+        #     for line_edit in zip(self.add_birthday_lines_ed, range(len(self.add_birthday_lines_ed))):
+        #         if len(line_edit[0].text()) == 0:
+        #             raise ValueError
+        #
+        #         if line_edit[1] == len(self.add_birthday_lines_ed) - 1:
+        #             lines_edit.append(datetime.strptime(line_edit[0].text(), "%d.%m.%Y"))
+        #         else:
+        #             lines_edit.append(line_edit[0].text())
+        #
+        #     self.db.add(lines_edit[0], lines_edit[1], lines_edit[2])
+        # except ValueError:
+        #     self.back_btn.hide()
+        #
+        #     warning_lbl = QLabel(self)
+        #     warning_lbl.setStyleSheet(self.labels_stylesheet)
+        #     warning_lbl.setText("You entered too big\nfirst name or last name,\nor date format was incorrect")
+        #     warning_lbl.move(110, 200)
+        #     warning_lbl.show()
+        #
+        #
+        #
+        #     self.back_button(self.back_btn, lambda: warning_lbl.hide())
+
     def add_birthday_btn_clicked(self):
-
-        def submit_btn_clicked():
-            try:
-                if len(date_le.text()) == 0 or len(last_name_le.text()) == 0 or len(first_name_le.text()) == 0:
-                    raise ValueError
-
-                date = datetime.strptime(date_le.text(), "%d.%m.%Y")
-                self.db.add(first_name_le.text(), last_name_le.text(), date)
-            except ValueError:
-                print("ValueError")
-                self.hide_widgets(*inner_btns.keys(), *inner_lse.keys(), *inner_lbls.keys())
-
-                warning_lbl = QLabel(self)
-                warning_lbl.setStyleSheet(self.labels_stylesheet)
-                warning_lbl.setText("You entered too big\nfirst name or last name,\nor date format was incorrect")
-                warning_lbl.move(110, 200)
-                warning_lbl.show()
-
-        def buttons():
-            self.set_buttons_settings(inner_btns)
-            self.show_widgets(*inner_btns.keys())
-            submit_btn.move(165, 300)
-
-        def labels():
-            x = 100
-            y = 160
-
-            self.set_labels_settings(inner_lbls)
-
-            for label in inner_lbls:
-                label.move(x, y)
-                y += self.labels_margin
-
-            self.show_widgets(*inner_lbls)
-
-        def lines_edit():
-            x = 240
-            y = 159
-
-            for line in inner_lse.keys():
-                line.setStyleSheet(self.lines_edit_stylesheet)
-                line.setPlaceholderText(inner_lse[line])
-                line.move(x, y)
-                y += self.lines_edit_margin
-
-            self.show_widgets(*inner_lse)
-
-        self.hide_widgets(*self.main_btns.keys(), *self.main_lbls)
-
-        submit_btn = QPushButton(self)
-
-        first_name_lbl = QLabel(self)
-        last_name_lbl = QLabel(self)
-        date_lbl = QLabel(self)
-
-        first_name_le = QLineEdit(self)
-        last_name_le = QLineEdit(self)
-        date_le = QLineEdit(self)
-
-        inner_btns = {submit_btn: ["Submit", submit_btn_clicked]}
-        inner_lbls = {first_name_lbl: "First name", last_name_lbl: "Last name", date_lbl: "Date"}
-        inner_lse = {first_name_le: "Vladimir", last_name_le: "Putin", date_le: "07.10.1952"}
-
-        buttons()
-        labels()
-        lines_edit()
+        self.main_window_gui_hide()
+        self.add_birthday_gui_show()
 
     def get_birthday_btn_clicked(self):
         pass
